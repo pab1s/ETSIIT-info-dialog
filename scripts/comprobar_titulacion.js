26600a53e29175d0a40f0e65218d2435569677ef
Pablo Olivares <pablolivares1502@gmail.com>
2023-10-03 13:33:21 +0200

    No funcionan las expresiones regulares

---
+++ scripts/comprobar_titulacion.js
@@ -0,0 +1,27 @@
+function reconocerTitulacion(texto) {
+    // Expresiones regulares para reconocer las titulaciones
+    const regexInformatica = /(ingenieria)? informatica/;
+    const regexTelecomunicaciones = /(ingenieria \s de \s tecnologia(s)? \s ((de)|(en) \s))? teleco(municacion(es)?)?/;
+    const regexADE = /(administracion y direccion de empresas | ade)/;
+    const regexDGIIM = /((doble)? (grado | titulacion) (en)?)? ((ingenieria)? \s informatica \s y? \s matematicas) | (matematicas (e|y)? (ingenieria)? informatica)/;
+    const regexDGIIADE = /((doble)? (grado | titulacion) (en)?)? ((ingenieria)? informatica y? (administracion y direccion de empresas | ade)) | ((administracion y direccion de empresas | ade) y? (ingenieria)? informatica)/;
+  
+    if (regexDGIIM.test(texto)) {
+      return "dgiim";
+    } else if (regexDGIIADE.test(texto)) {
+      return "dgiiade";
+    } else if (regexInformatica.test(texto)) {
+      return "informatica";
+    } else if (regexTelecomunicaciones.test(texto)) {
+      return "teleco";
+    } else {
+      return "error"; // Si no se reconoce ninguna titulación, devolvemos 0
+    }
+  }
+  
+  // Ejemplo de uso
+  const texto = "teleco";
+  const nombreTitulacion = reconocerTitulacion(texto);
+  
+  console.log("Titulación:", nombreTitulacion);
+  
