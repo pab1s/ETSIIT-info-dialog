ae27572dc7b2ea129a0796ec020b1bfc14c05242
Pablo Olivares <pablolivares1502@gmail.com>
2023-10-03 23:56:05 +0200

    Scripts actualizados

--- scripts/server_template.php
+++ scripts/server_template.php
@@ -1,14 +1,15 @@
 <?php
 // Get the day of the week from the VXML POST request
-$dayOfWeek = $_POST['dayOfWeek'];
+// $profesor = $_POST['profesor'];
+$profesor = "Marcelino José Cabrera Cuevas";
 
 // Perform a database query to retrieve the menu for the specified day
 // Replace these with your database connection details
-$serverName = "your_server_name";
+$serverName = "sql11.freesqldatabase.com";
 $connectionOptions = array(
-    "Database" => "menus",
-    "Uid" => "your_username",
-    "PWD" => "your_password"
+    "Database" => "sql11650278",
+    "Uid" => "sql11650278",
+    "PWD" => "Akie4puyXm"
 );
 
 $conn = sqlsrv_connect($serverName, $connectionOptions);
@@ -17,8 +18,8 @@ if (!$conn) {
     die("Connection failed: " . sqlsrv_errors());
 }
 
-$sql = "SELECT menu FROM daily_menus WHERE day_of_week = ?";
-$params = array($dayOfWeek);
+$sql = "SELECT asignatura FROM ugr_courses WHERE profesor = ?";
+$params = array($profesor);
 $query = sqlsrv_query($conn, $sql, $params);
 
 if ($query === false) {
@@ -29,10 +30,10 @@ $row = sqlsrv_fetch_array($query, SQLSRV_FETCH_ASSOC);
 
 if (!$row) {
     // If there's no menu for the specified day, you can provide a response accordingly
-    $menuResponse = "I'm sorry, there is no menu available for $dayOfWeek.";
+    $response = "Disculpa, no hemos encontrado ninguna asignatura impartida por $profesor.";
 } else {
     // Get the menu from the database
-    $menuResponse = $row['menu'];
+    $response = $row['asignatura'];
 }
 
 // Output the menu as VoiceXML
@@ -41,7 +42,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>
 <vxml version="2.1" xmlns="http://www.w3.org/2001/vxml">
   <form>
     <block>
-      <prompt>' . $menuResponse . '</prompt>
+      <prompt>' . $response . '</prompt>
     </block>
   </form>
 </vxml>';
