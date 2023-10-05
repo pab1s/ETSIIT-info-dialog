94bbd1e165c52eca1cde496fdded9286d33d307f
luiscrespoo <80181219+luiscrespoo@users.noreply.github.com>
2023-10-05 12:04:38 +0200

    ahora el script devuelve un string

--- scripts/checkProfesores.js
+++ scripts/checkProfesores.js
@@ -1,30 +1,32 @@
 const fs = require('fs');
 const csvParser = require('csv-parser');
 
-function checkProfesores(asignatura, titulacion) {
-    let profesoresAsociados = new Set();
+async function checkProfesores(asignatura, titulacion) {
+    return new Promise((resolve, reject) => {
+        let profesoresAsociados = new Set();
 
-    fs.createReadStream('../data/ugr_data.csv')
-        .pipe(csvParser({ separator: ',' })) // Ajusta el delimitador según tu archivo CSV
-        .on('data', (row) => {
-            if(row.Titulacion.trim() === asignatura && row.Asignatura.trim() === titulacion){
-                profesoresAsociados.add(row.Profesor && row.Profesor.trim());
-            }
-        })
-        .on('end', () => {
-            if (profesoresAsociados.size > 0) {
-                console.log('Profesor(es) asociado(s) a la asignatura y titulación:');
-                console.log(profesoresAsociados);
-            } else {
-                console.log('Ningún profesor encontrado para la asignatura y titulación especificadas.');
-            }
-        })
-        .on('error', (error) => {
-            console.error('Error al leer el archivo CSV:', error);
-        });
+        fs.createReadStream('../data/ugr_data.csv')
+            .pipe(csvParser({ separator: ',' }))
+            .on('data', (row) => {
+                if (row.Titulacion.trim() === asignatura && row.Asignatura.trim() === titulacion) {
+                    profesoresAsociados.add(row.Profesor && row.Profesor.trim());
+                }
+            })
+            .on('end', () => {
+                if (profesoresAsociados.size > 0) {
+                    let profesoresString = [...profesoresAsociados].join(', ');
+                    if (profesoresAsociados.size > 1) {
+                        const lastCommaIndex = profesoresString.lastIndexOf(', ');
+                        profesoresString = `${profesoresString.substring(0, lastCommaIndex)} y${profesoresString.substring(lastCommaIndex + 1)}`;
+                    }
+                    resolve(profesoresString);
+                } else {
+                    resolve('Ningún profesor encontrado para la asignatura y titulación especificadas.');
+                }
+            })
+            .on('error', (error) => {
+                reject('Error al leer el archivo CSV: ' + error);
+            });
+    });
 }
 
-// Ejemplo de uso
-const asignatura = 'Grado en Ingenieria Informatica';
-const titulacion = 'Inteligencia Artificial';
-checkProfesores(asignatura, titulacion);
