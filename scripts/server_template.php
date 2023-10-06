5660c4725f7350441d1b4780bc3438c334ce3b84
Pablo Olivares <pablolivares1502@gmail.com>
2023-10-06 09:03:37 +0200

    Servidor funcional

--- scripts/server_template.php
+++ scripts/server_template.php
@@ -1,52 +1,58 @@
 <?php
-  // Get the day of the week from the VXML POST request
-  $profesor = $_POST['profesor'];
-  /*
-  // Perform a database query to retrieve the menu for the specified day
-  // Replace these with your database connection details
-  $serverName = "sql11.freesqldatabase.com";
-  $connectionOptions = array(
-    "Database" => "sql11650278",
-    "Uid" => "sql11650278",
-    "PWD" => "Akie4puyXm"
-  );
+// Set the content type to VoiceXML
+header('Content-type: application/voicexml+xml');
 
-  $conn = sqlsrv_connect($serverName, $connectionOptions);
+// Get the day of the week from the VXML POST request
+$profesor = "Pablo Mesejo Santiago"; // $_POST['profesor'];
 
-  if (!$conn) {
-    die("Connection failed: " . sqlsrv_errors());
-  }
+$serverName = "sql11.freesqldatabase.com";
+$database = "sql11650278";
+$username = "sql11650278";
+$password = "Akie4puyXm";
 
-  $sql = "SELECT asignatura FROM etsiit_courses WHERE profesor = ?";
-  $params = array($profesor);
-  $query = sqlsrv_query($conn, $sql, $params);
+try {
+    $conn = new PDO("mysql:host=$serverName;dbname=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo "Connected successfully to MySQL"; // Commented out for cleaner output
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
 
-  if ($query === false) {
-    die("Query failed: " . sqlsrv_errors());
-  }
+// Use prepared statement to avoid SQL injection
+try {
+    $sql = "SELECT DISTINCT asignatura FROM etsiit_courses WHERE profesor = :profesor";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':profesor', $profesor, PDO::PARAM_STR);
+    $stmt->execute();
 
-  $row = sqlsrv_fetch_array($query, SQLSRV_FETCH_ASSOC);
+    $coursesFound = false; // Initialize a flag to track if courses were found
+    $courseList = array(); // Initialize an array to store course names
 
-  if (!$row) {
-    // If there's no menu for the specified day, you can provide a response accordingly
-    $response = "Disculpa, no hemos encontrado ninguna asignatura impartida por $profesor.";
-  } else {
-    // Get the menu from the database
-    // $response = $row['asignatura'];
-    $response = "Hemos encontrado su respuesta";
-  }
-  */
-  // Output the menu as VoiceXML
-  echo("<?vxml version=\"1.0\"?>\n");
-  $string = "Hello XIMO! How are you?";
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        // Process each row of data
+        $courseList[] = $row['asignatura']; // Add the course to the list
+        $coursesFound = true; // Set the flag to true if at least one course is found
+    }
 
-  // Close the database connection
-  // sqlsrv_close($conn);
+    if (!$coursesFound) {
+        // If there are no results for the specified professor, provide a response accordingly
+        $response = "Disculpa, no hemos encontrado ninguna asignatura impartida por $profesor.";
+    } else {
+        // If courses were found, set the response to include the list of courses
+        $courseListStr = implode(", ", $courseList); // Convert the array to a comma-separated string
+        $response = "Hemos encontrado asignatura(s) impartida(s) por $profesor. Las asignaturas son: $courseListStr.";
+    }
+} catch (PDOException $e) {
+    echo "Query failed: " . $e->getMessage();
+}
+
+// Output the response as VoiceXML
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+echo '<vxml version="2.1" xml:lang="es-ES" xmlns="http://www.w3.org/2001/vxml">';
+echo '<form>';
+echo '<block>';
+echo "<prompt>$response</prompt>";
+echo '</block>';
+echo '</form>';
+echo '</vxml>';
 ?>
-<vxml version="2.1" xmlns="http://www.w3.org/2001/vxml">
-  <form>
-    <block>
-      <prompt><?=htmlspecialchars($string)?>.</prompt>
-    </block>
-  </form>
-</vxml>
