79a3bbf9e84d5d0ddd77b45d900b195dc0fcf663
moha-ln1 <mohamedln@corre.ugr.es>
2023-10-06 16:43:48 +0200

    He añadido el script para asignaturas

---
+++ scripts/create_asignaturas_grammar.py
@@ -0,0 +1,27 @@
+import csv
+from unidecode import unidecode
+
+# Leer los nombres de asignaturas desde el archivo CSV y almacenarlos en un conjunto para evitar duplicados
+asignaturas_set = set()
+
+with open('ugr_data.csv', 'r', newline='', encoding='utf-8') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        asignatura = row['asignatura']
+        # Aplicar unidecode para eliminar tildes y caracteres especiales
+        asignatura_sin_tilde = unidecode(asignatura)
+        # Agregar el profesor al conjunto
+        asignaturas_set.add(asignatura_sin_tildes.strip())  # Eliminar espacios en blanco al principio y al final del nombre
+
+# Crear el archivo .jsgf con los nombres de asignaturas sin repeticiones y sin tildes
+with open('asignaturas.jsgf', 'w', encoding='utf-8') as jsgf_file:
+    jsgf_file.write('''
+#JSGF V1.0;
+
+grammar asignaturas;
+
+public <asignaturas> = {}
+'''.format(' | '.join(asignaturas_set)))
+
+print("Archivo asignaturas.jsgf creado exitosamente con los nombres de asignaturas sin repeticiones y sin tildes.")
+
