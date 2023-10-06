bd4b51d19b92f974c4e154e04db46f5c1b41b4ed
Pablo Olivares <pablolivares1502@gmail.com>
2023-10-06 19:07:06 +0200

    Añade especialidad a SQL DB

--- scripts/db_creation.sql
+++ scripts/db_creation.sql
@@ -1,13 +1,14 @@
 CREATE TABLE ugr_courses (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    titulacion VARCHAR(255),
-    asignatura VARCHAR(255),
-    curso VARCHAR(10),
-    semestre VARCHAR(255),
-    grupo VARCHAR(10),
-    tipo_grupo VARCHAR(255),
-    profesor VARCHAR(255),
-    dia_de_la_semana VARCHAR(255),
+    indice INT AUTO_INCREMENT PRIMARY KEY,
+    titulacion VARCHAR(50),
+    asignatura VARCHAR(50),
+    especialidad VARCHAR(50),
+    curso VARCHAR(50),
+    semestre VARCHAR(50),
+    grupo VARCHAR(50),
+    tipo_grupo VARCHAR(50),
+    profesor VARCHAR(50),
+    dia_de_la_semana VARCHAR(50),
     hora_inicio TIME,
     hora_fin TIME
 );
