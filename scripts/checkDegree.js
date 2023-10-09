c1e3a045ff514583f593cc4ff8acc1978d211af3
Ximo Sanz Tornero <ximosanz2001@gmail.com>
2023-10-09 19:05:57 +0200

    cambio comillas

--- scripts/checkDegree.js
+++ scripts/checkDegree.js
@@ -1,56 +1,56 @@
 function checkDegree(degree) {
   const informatics = [
-    "informatics",
-    "computer science",
-    "computer engineering",
-    "bachelors degree in computer engineering",
-    "bachelor in computer engineering",
-    "bachelors degree in computer science"
+    'informatics',
+    'computer science',
+    'computer engineering',
+    'bachelors degree in computer engineering',
+    'bachelor in computer engineering',
+    'bachelors degree in computer science'
   ];
   
   const telecommunications = [
-    "teleco",
-    "telecommunications",
-    "bachelors degree in telecommunications",
-    "bachelor in telecommunications"
+    'teleco',
+    'telecommunications',
+    'bachelors degree in telecommunications',
+    'bachelor in telecommunications'
   ];
   
   const business = [
-    "business",
-    "management",
-    "administration",
-    "informatics and business",
-    "business and informatics",
-    "informatics and business administration",
-    "business administration and computer science",
-    "computer science and business administration",
-    "computer engineering and business administration",
-    "business administration and computer engineering",
-    "computer engineering and management",
-    "computer engineering and business management",
-    "bachelors degree in computer engineering and bachelors degree in business administration and management",
-    "bachelors degree in computer engineering plus bachelors degree in business administration and management",
-    "double degree in computer science and business administration",
-    "double degree in computer engineering and business administration",
-    "double degree in informatics and ade",
-    "double degree in computer engineering and ade"
+    'business',
+    'management',
+    'administration',
+    'informatics and business',
+    'business and informatics',
+    'informatics and business administration',
+    'business administration and computer science',
+    'computer science and business administration',
+    'computer engineering and business administration',
+    'business administration and computer engineering',
+    'computer engineering and management',
+    'computer engineering and business management',
+    'bachelors degree in computer engineering and bachelors degree in business administration and management',
+    'bachelors degree in computer engineering plus bachelors degree in business administration and management',
+    'double degree in computer science and business administration',
+    'double degree in computer engineering and business administration',
+    'double degree in informatics and ade',
+    'double degree in computer engineering and ade'
   ];
   
   const maths = [
-    "maths",
-    "mathematics",
-    "informatics and maths",
-    "informatics and mathematics",
-    "computer science and maths",
-    "computer engineering and maths",
-    "computer science and mathematics",
-    "computer engineering and mathematics",
-    "double degree in computer engineering and mathematics",
-    "double degree in computer engineering and maths",
-    "double degree in mathematics and computer engineering",
-    "bachelors degree in computer engineering and mathematics",
-    "bachelors degree in computer engineering and bachelors degree in mathematics",
-    "bachelors degree in computer engineering plus bachelors degree in mathematics"
+    'maths',
+    'mathematics',
+    'informatics and maths',
+    'informatics and mathematics',
+    'computer science and maths',
+    'computer engineering and maths',
+    'computer science and mathematics',
+    'computer engineering and mathematics',
+    'double degree in computer engineering and mathematics',
+    'double degree in computer engineering and maths',
+    'double degree in mathematics and computer engineering',
+    'bachelors degree in computer engineering and mathematics',
+    'bachelors degree in computer engineering and bachelors degree in mathematics',
+    'bachelors degree in computer engineering plus bachelors degree in mathematics'
   ];
   
   let response = 0;
